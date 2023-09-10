e71a650a6074a09daf7bea5b550293469c39c260
cpt_wish <batuhandlek@gmail.com>
2023-09-10 16:24:57 +0300

    update brutus

--- brutus.py
+++ brutus.py
@@ -24,18 +24,34 @@ class Brutus:
         self.analitics = [8.4966,2.0720,4.5388,3.3844,11.1607,1.8121,2.4705,3.0034,7.5448,0.1965,1.1016,5.4893,3.0129,6.6544,7.1635,
                           3.1671,0.1962,7.5809,5.7351,6.9509,3.6308,1.0074,1.2899,0.2902,1.7779,0.2722]
         
-        
+
+    def findBiggestIndexes(self):
+        copyAnalytic = self.analitics
+        result = []
+        for maxCount in range(0, len(self.analitics)- 6):
+            max = 0
+            for ent in copyAnalytic:
+                if ent > max:
+                    max = ent
+
+            result.append(self.analitics.index(max))
+            copyAnalytic[copyAnalytic.index(max)] = 0     
+        return result               
+
+
     def cracker(self, text):
         # y - k mod 26
         tmpList = list(text)
         wordCountofText = []
         hitCount = 0
-        biggestThreeOriginal = [4, 0, 17]
+        biggestOriginal = self.findBiggestIndexes()
         sortedWordIndexes = []
-        biggestThree = [0,0,0]
+        biggest = []
+        for i in range(0, self.keySpace):
+            biggest.append(0)
         moduloRes = 0
         keyProbs = []
-        possibleKeys = [0,0,0]
+        possibleKeys = [0,0,0,0,0]
         for j in range (0, self.keySpace):
             wordCountofText.append(0)
             keyProbs.append(0)
@@ -76,22 +92,22 @@ class Brutus:
                 wordCountofText[index] = wordCountofText[index] + 1
             #print("Wordcounts " + str(wordCountofText))
             #find biggest three in text
-            biggestThree[0] = 0
-            for maxCount in range(0,3):
+            biggest[0] = 0
+            for maxCount in range(0,self.keySpace):
                 max1 = 0
                 for wordIndex in range(0, len(wordCountofText)):
                     if wordCountofText[wordIndex] > max1:
                         max1 = wordCountofText[wordIndex]
 
-                biggestThree[maxCount] = wordCountofText.index(max1)
+                biggest[maxCount] = wordCountofText.index(max1)
                 wordCountofText[wordCountofText.index(max1)] = 0
 
             
-            #print("Biggest indexes of the alphabet are : \n" + str(biggestThree))
+            #print("Biggest indexes of the alphabet are : \n" + str(biggest))
             #check hits
-            for p in biggestThree:
-                #if i in lst
-                if p in biggestThreeOriginal:
+            for p in biggest:
+               
+                if p in biggestOriginal:
                     hitCount = hitCount + 1
 
             keyProbs[k] = hitCount
@@ -99,11 +115,11 @@ class Brutus:
             #print("Key probs : " + str(keyProbs))    
             #print("-------------------------------")
         #print("Key probs : " + str(keyProbs))
-        print("Calculating the best keys[ ]")
+        print("\n\n\nCalculating the best keys[ ]")
         copyOfKeyProbs = keyProbs
-        print(keyProbs)
+        #print(keyProbs)
         possibleKeys[0] = keyProbs[0]
-        for p in range(0,3):
+        for p in range(0,5):
             max = 0
             for key in range(0, len(keyProbs)):
                 if copyOfKeyProbs[key] > max:
@@ -114,6 +130,7 @@ class Brutus:
 
         print("Best possible keys are : ")
         print(possibleKeys)
+        print()
         
 
         
@@ -137,7 +154,7 @@ class Brutus:
                         
                         
             last = "".join(tmpList)
-            print("Key : " + str(k) + " Encrypted message : " + last)
+            print("Key : " + str(k) + " Decrypted message : " + last + "\n")
 
 
 a = Brutus()
